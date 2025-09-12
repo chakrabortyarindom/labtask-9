fbe72225c7b204ee60cae17f76ef198f39126c80
chakrabortyarindom <arindomc628@gmail.com>
2025-09-12 18:44:58 +0600

    Add files via upload

---
+++ hckertrank11to30.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class hckertrank11to30 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>= -100 && n<=100){
+            System.out.println("GOOD JOB");
+        }
+        else{
+            System.out.println("BAD JOB");
+        }
+    }
+}
+
+
