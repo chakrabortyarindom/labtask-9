1a7ce2eacda2cfe8f69d8f2d26db383186777413
chakrabortyarindom <arindomc628@gmail.com>
2025-09-15 10:26:07 +0600

    Add files via upload

---
+++ onedarr.java
@@ -0,0 +1,16 @@
+public class onedarr {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+
+        System.out.println("First element: " + arr[0]);
+        System.out.println("Third element: " + arr[2]);
+
+      
+        System.out.println("All elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
